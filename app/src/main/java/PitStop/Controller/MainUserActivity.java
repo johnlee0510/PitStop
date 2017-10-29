37b2bb696a24fa37b63cd408ddc6088fb9e9b5f2
Dorkly <bree_cakesx3@live.com>
2017-10-29 01:01:45 -0400

    Menu Additions

--- app/src/main/java/PitStop/Controller/MainUserActivity.java
+++ app/src/main/java/PitStop/Controller/MainUserActivity.java
@@ -2,6 +2,7 @@ package PitStop.Controller;
 /**
  * Created by John on 10/28/2017.
  */
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
