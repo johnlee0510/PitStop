5c114020dd21373134f5cf34969051b0cb2e3bb0
Dorkly <bree_cakesx3@live.com>
2017-10-29 03:06:51 -0400

    UI Edits

--- app/src/main/java/PitStop/Controller/CurrentTab.java
+++ app/src/main/java/PitStop/Controller/CurrentTab.java
@@ -3,7 +3,6 @@ package PitStop.Controller;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -65,6 +64,7 @@ public class CurrentTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_current_tab, container, false);
     }
