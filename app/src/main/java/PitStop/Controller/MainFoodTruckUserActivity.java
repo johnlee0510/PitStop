143f7dd44ebc6ed2c3d666ec57a9f81927c3c73f
Dorkly <bree_cakesx3@live.com>
2017-10-29 00:35:01 -0400

    Menu Adding

--- app/src/main/java/PitStop/Controller/MainFoodTruckUserActivity.java
+++ app/src/main/java/PitStop/Controller/MainFoodTruckUserActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import PitStop.R;
@@ -17,6 +19,19 @@ public class MainFoodTruckUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_food_truck_user);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
     }
 
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.truckmenu, menu);
+
+        return true;
+
+    }
 }
--- app/src/main/java/PitStop/Controller/MainUserActivity.java
+++ app/src/main/java/PitStop/Controller/MainUserActivity.java
@@ -5,6 +5,8 @@ package PitStop.Controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import PitStop.R;
@@ -19,6 +21,17 @@ public class MainUserActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
     }
 
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.usermenu, menu);
+
+        return true;
+
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
