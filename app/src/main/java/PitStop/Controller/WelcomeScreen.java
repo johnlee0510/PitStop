0524853cf053ee2f4b5de6e6398ba8087105212c
Dorkly <bree_cakesx3@live.com>
2017-10-28 21:10:06 -0400

    Welcome Screen Ui Edits

--- app/src/main/java/PitStop/Controller/WelcomeScreen.java
+++ app/src/main/java/PitStop/Controller/WelcomeScreen.java
@@ -1,7 +1,10 @@
 package PitStop.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import PitStop.R;
 
@@ -11,5 +14,33 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        // Configure Login Button
+        Button loginButton = (Button) findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                loginClicked();
+            }
+        });
+
+        // Configure Registration Button
+        Button regButton = (Button) findViewById(R.id.register);
+        regButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                registrationClicked();
+            }
+        });
+    }
+
+    private void loginClicked() {
+        Intent intent = new Intent(WelcomeScreen.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void registrationClicked() {
+        Intent intent2 = new Intent(WelcomeScreen.this, RegistrationActivity.class);
+        startActivity(intent2);
     }
 }
