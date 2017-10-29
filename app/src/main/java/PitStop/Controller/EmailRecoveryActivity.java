4a3603463caf18859e71ff80d908f9d84f8cbd1b
AustinJ's Mac <AustinJ@lawn-128-61-28-192.lawn.gatech.edu>
2017-10-29 00:20:50 -0400

    More Actvity

---
+++ app/src/main/java/PitStop/Controller/EmailRecoveryActivity.java
@@ -0,0 +1,15 @@
+package PitStop.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.john.pitstop.R;
+
+public class EmailRecoveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_recovery);
+    }
+}
