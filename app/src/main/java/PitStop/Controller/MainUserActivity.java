519419357d972db79e7091588dac01096c389c87
Dorkly <bree_cakesx3@live.com>
2017-10-28 22:02:26 -0400

    Menus

--- app/src/main/java/PitStop/Controller/MainUserActivity.java
+++ app/src/main/java/PitStop/Controller/MainUserActivity.java
@@ -5,6 +5,7 @@ package PitStop.Controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import PitStop.R;
 
@@ -16,5 +17,25 @@ public class MainUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_user);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_profile:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_truck:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
