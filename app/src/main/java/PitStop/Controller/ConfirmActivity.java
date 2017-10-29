e59edad152bf35febd297920b92ec552561fd70d
Dorkly <bree_cakesx3@live.com>
2017-10-29 07:45:10 -0400

    added Confirm page

---
+++ app/src/main/java/PitStop/Controller/ConfirmActivity.java
@@ -0,0 +1,19 @@
+package PitStop.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import PitStop.R;
+
+public class ConfirmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar6);
+        myToolbar.setTitleTextColor(android.graphics.Color.WHITE);
+        setSupportActionBar(myToolbar);
+    }
+}
